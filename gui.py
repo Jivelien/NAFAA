ae35368abac59ee50121fde9c300a3de49a6efa0
Jivelien <julien.Delannoy@hotmail.com>
2020-12-09 23:34:29 +0100

    starting to use uic

--- gui.py
+++ gui.py
@@ -4,7 +4,11 @@ from PyQt5.QtWidgets import (QWidget, QLabel,
                              QComboBox, QApplication, QPushButton, QLCDNumber)
 from PyQt5.QtCore import QBasicTimer
 from PyQt5.QtGui import QPixmap
-
+from PyQt5.QtWidgets import (QWidget, QSlider, QLineEdit, QLabel, QPushButton, QScrollArea,QApplication,
+                             QHBoxLayout, QVBoxLayout, QMainWindow)
+from PyQt5.QtCore import Qt, QSize
+from PyQt5 import QtWidgets, uic
+import sys
 
 class MainWindow(QWidget):
 
@@ -12,8 +16,8 @@ class MainWindow(QWidget):
         super().__init__()
         self.daemon = TrainingDaemon(delay=1, debug=True)
         self.initUI()
-        self.timer = QBasicTimer()
-        self.timer.start(10, self)
+        # self.timer = QBasicTimer()
+        # self.timer.start(10, self)
 
     def timerEvent(self, e):
         picture_path = self.daemon.current_exercise.exercice.picture_path if self.daemon.current_exercise is not None else None
@@ -27,20 +31,24 @@ class MainWindow(QWidget):
 
     def initUI(self):
 
-        self.program_list = QComboBox(self)
-        self.qbtn = QPushButton('STATE', self)
-        self.lbl = QLabel('pic here', self)
-        self.lbl.setGeometry(100,100, 600, 400)
-        self.timer_step = QLCDNumber(self)
-        self.timer_step.setGeometry(100,0, 200, 50)
+        # self.program_list = QComboBox(self)
+        # self.qbtn = QPushButton('STATE', self)
+        # self.lbl = QLabel('pic here', self)
+        # self.lbl.setGeometry(100,100, 600, 400)
+        # self.timer_step = QLCDNumber(self)
+        # self.timer_step.setGeometry(100,0, 200, 50)
 
 
-        for key in self.daemon.program_list.keys():
-            self.program_list.addItem(key)
-        self.program_list.move(50, 50)
-        self.program_list.activated[str].connect(self.select_program)
-        self.qbtn.clicked.connect(self.state_button)
-        self.setGeometry(300, 300, 450, 400)
+        self.scroll = QScrollArea(self)
+        self.scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.scroll.setWidgetResizable(True)
+
+        self.scroll_Layout = QtWidgets.QVBoxLayout(self.scroll)
+        for _ in range(12):
+            self.scroll_Layout.addWidget(QLabel('test'))
+
+        self.setGeometry(300, 300, 1000, 400)
         self.setWindowTitle('Never A Fat Ass Again')
         self.show()
 
